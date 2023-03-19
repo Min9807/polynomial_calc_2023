245ad5e32a0b5f2ce20840f66b2d7c88f6293639
Jeongmin Lee <dlwogns9807@naver.com>
2023-03-19 23:51:23 +0900

    (10 + 20) * 3 == 90

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -126,4 +126,10 @@ public class CalcTest {
     void t20() {
         assertThat(Calc.run("(20 + 20) + 20")).isEqualTo(60);
     }
+
+    @Test
+    @DisplayName("(10 + 20) * 3 == 90")
+    void t21() {
+        assertThat(Calc.run("(10 + 20) * 3")).isEqualTo(90);
+    }
 }
