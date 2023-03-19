f8d9c4fce93d484442eedaa39b763531fe952268
Jeongmin Lee <dlwogns9807@naver.com>
2023-03-20 00:10:08 +0900

    10 + (10 + 5) == 25

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -132,4 +132,10 @@ public class CalcTest {
     void t21() {
         assertThat(Calc.run("(10 + 20) * 3")).isEqualTo(90);
     }
+
+    @Test
+    @DisplayName("10 + (10 + 5) == 25")
+    void t22() {
+        assertThat(Calc.run("10 + (10 + 5)")).isEqualTo(25);
+    }
 }
